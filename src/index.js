205467a4d6b624f3c43d3465cc40a1d72c01104c
silvawilliam <silva.william047@gmail.com>
2026-02-18 20:41:24 -0300

    Criando novas Rotas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello");
+import express from 'express';
+import *as dotenv from 'dotenv';
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Criar nossa rotas
+
+// GET http://localhost:3000/teste
+app.get("/teste", (req, res) => {
+    console.log("Hello");
+
+    res.send("Hello response");
+    
+});
+
+const porta = process.env.PORT;
+app.listen(porta, () => {
+    console.log("O servidor está funcionando"+ porta);
+});
+
