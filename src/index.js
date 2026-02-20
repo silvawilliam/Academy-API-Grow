ca92d484cc0281216ef283c37a7cad018c4848c4
silvawilliam <silva.william047@gmail.com>
2026-02-19 22:27:57 -0300

    Adicionado Rotas

--- src/index.js
+++ src/index.js
@@ -7,12 +7,29 @@ app.use(express.json());
 
 // Criar nossa rotas
 
+/*
 // GET http://localhost:3000/teste
 app.get("/teste", (req, res) => {
     console.log("Hello");
 
     res.send("Hello response");
     
+});
+*/
+
+app.get("/hello",(req, res) => {
+    res.send("Exercício Feito")
+});
+
+app.get("/about", (req, res) => {
+    res.send({
+        nome: "William Silva",
+        email:"silva@gmail.com",
+        resumo: "Mentor",
+        idade: 35,
+        skills:["Node.js", "API REST, Express"]
+
+    })
 });
 
 const porta = process.env.PORT;
